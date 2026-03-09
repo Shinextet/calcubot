1b77e17e488862a23329869b1a5e98aa6803aaff
Shinextet <147969251+Shinextet@users.noreply.github.com>
2026-03-09 16:36:48 +0630

    Update calcu.py

--- calcu.py
+++ calcu.py
@@ -4,6 +4,10 @@ import re
 TOKEN = '8562810091:AAF774c8Es0zv0nGRSQ1xIZjZqhvd3LPh7I' # ဒီနေရာမှာ သင့် Bot Token ထည့်ပါ
 bot = telebot.TeleBot(TOKEN)
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "သင်္ချာပုစ္ဆာတစ်ခုခု ရိုက်ထည့်လိုက်ပါ (ဥပမာ- 1+1)")
+
 @bot.message_handler(func=lambda message: True)
 def calculate(message):
     try:
@@ -18,7 +22,7 @@ def calculate(message):
             answer = eval(expression)
             bot.reply_to(message, f"ရလဒ်: {answer}")
         else:
-            bot.reply_to(message, "တွက်ချက်ဖို့ သင်္ချာပုံစံ (ဥပမာ 5750*2) ရှာမတွေ့ပါဘူး။")
+            bot.reply_to(message, "တွက်ချက်ဖို့ သင်္ချာပုံစံ (ဥပမာ 1*2) ရှာမတွေ့ပါဘူး။")
             
     except Exception:
         bot.reply_to(message, "တွက်ချက်မှု မှားယွင်းနေပါတယ်။")
